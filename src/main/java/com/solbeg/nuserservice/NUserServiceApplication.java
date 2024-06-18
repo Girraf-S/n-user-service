6b463c2dd369aeb44bcc4f639a13d64ef29af674
Sciapan Hembar <hsciapan@gmail.com>
2024-06-18 17:38:13 +0300

    P-1: Init project

---
+++ src/main/java/com/solbeg/nuserservice/NUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solbeg.nuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solbeg/nuserservice/NUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solbeg.nuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
