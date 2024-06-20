f80b74509ea9173f967d6749d869f9efd99266d3
Sciapan Hembar <hsciapan@gmail.com>
2024-06-20 11:22:51 +0300

    P-1: Add create-admin script

---
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -0,0 +1,10 @@
+insert into users (first_name,
+                   last_name,
+                   password,
+                   email,
+                   role)
+values ('Anakin',
+        'Skywalker',
+        'choosenOne2000',
+        'hsciapan@gmail.com',
+        0)
